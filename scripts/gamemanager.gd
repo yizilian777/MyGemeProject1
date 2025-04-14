a91f393e8655a697d3be2e041f9d9ef9ad2f14eb
yizilian777 <is0757hi@ed.ritsumei.ac.jp>
2025-04-14 10:12:18 +0900

    增加了怪物

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @export var slime_scene : PackedScene
-@export var spawn_timer : Timer
+@export var slime_timer : Timer
 
 @export var dino_scene : PackedScene
 @export var dino_timer : Timer
 
+@export var dog_scene: PackedScene
+@export var dog_timer : Timer
 
 @export var score : int = 0
 @export var score_label : Label
@@ -15,35 +17,38 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#dino生成开始
-	await get_tree().create_timer(7).timeout
+	#怪物生成开始时间
+	await get_tree().create_timer(1).timeout
 	dino_timer.start()
+	
+	await get_tree().create_timer(2).timeout
+	dog_timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	spawn_timer.wait_time -= 0.2 * delta
-	spawn_timer.wait_time = clamp(spawn_timer.wait_time, 0.5, 3)
+	#怪物生成间隔
+	slime_timer.wait_time -= 0.2 * delta
+	slime_timer.wait_time = clamp(slime_timer.wait_time, 0.5, 3)
 
 	score_label.text = "Score: " + str(score)
 
+
+#生成怪物
+func _spawn_enemy(enemy_scene: PackedScene) -> void:
+	var enemy = enemy_scene.instantiate()
+	enemy.position = Vector2(260, randf_range(50, 120))
+	enemy.player = player
+	add_child(enemy)
+
 func _spawn_slime() -> void:
-	var slime_node =slime_scene.instantiate()
-	slime_node.position = Vector2(260, randf_range(50, 120))
-	
-	slime_node.player = player
-	
-	get_tree().current_scene.add_child(slime_node)
- 
+	_spawn_enemy(slime_scene)
 
 func _spawn_dino() -> void:
-	var dino_node =dino_scene.instantiate()
-	dino_node.position = Vector2(260, randf_range(50, 120))
+	_spawn_enemy(dino_scene)
 	
-	dino_node.player = player
-	
-	get_tree().current_scene.add_child(dino_node)
- 
+func _spawn_dog() -> void:
+	_spawn_enemy(dog_scene)
 	
 func show_game_over():
 	game_over_label.visible = true
